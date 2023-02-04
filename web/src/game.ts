27b6b765e52a72e5667823be4e0c9d3d1d6bf49d
Philipp Kübler <info@pkuebler.de>
2023-02-04 20:55:31 +0100

    remove old code

--- web/src/game.ts
+++ web/src/game.ts
@@ -324,30 +324,7 @@ export class Game {
 				return;
 			}
 		});
-/*
-		this.map.rooms.forEach((roomT) => {
-			if (roomT.position.y !== position.y) {
-				return;
-			}
-			let withcheck = this._config.rooms[roomT.type].width;
-			if (roomT.position.x === position.x) {
-				console.log(
-					`Room already exists at position X:${position.x} Y:${position.y}`,
-				);
-				canbuild = false;
-				return;
-			}
-			for (let i = 1; i < withcheck; i++) {
-				if (roomT.position.x === position.x - i) {
-					console.log(
-						`Room already exists at position X:${position.x} Y:${position.y}`,
-					);
-					canbuild = false;
-					return;
-				}
-			}
-		});
-		*/
+
 		if (canbuild && isRoomNearBy) {
 			this._resources.wood -= config.rooms[room.type].costs.wood;
 			this._resources.stone -= config.rooms[room.type].costs.stone;
